61009d513fb01efc321a84da05acd5817a835237
AngelLM <alm37454@gmail.com>
2017-07-30 17:04:02 +0200

    added some unitary tests

---
+++ PythonUnitaryTests/hackaday.py
@@ -0,0 +1,58 @@
+import bs4 as bs
+import urllib2
+
+HDurl = 'https://hackaday.io/AngelLM'
+preurl = 'https://hackaday.io'
+urlList=[]
+HDlikes = 0
+HDviews = 0
+HDcomments = 0
+HDprojfollowers = 0
+
+req = urllib2.Request(HDurl, headers={ 'User-Agent': 'Mozilla/5.0' })
+source = urllib2.urlopen(req).read()
+soup = bs.BeautifulSoup(source,'lxml')
+
+HDproffollowers = int(soup.find_all('span', {'class':'followers'})[0].text)
+
+projectSection = soup.find_all('section', {'class':'section'})[4]
+projectItems = projectSection.find_all('div', {'class':'project-item'})
+
+for each in projectItems:
+    projectUrl = each.findAll('a', {'class':'item-link'})[0]
+    urlList.append(projectUrl.get('href'))
+
+
+
+for i in range (len(urlList)):
+    newUrl = preurl + urlList[i]
+    req2 = urllib2.Request(newUrl, headers={ 'User-Agent': 'Mozilla/5.0' })
+    source2 = urllib2.urlopen(req2).read()
+    soup2 = bs.BeautifulSoup(source2,'lxml')
+
+    likesRaw = soup2.findAll("span", {'class':'like_count'})[0].text
+    if 'k' in likesRaw:
+        likesRaw = float(likesRaw.replace("k", ""))*1000
+    HDlikes += int(likesRaw)
+
+    viewsRaw = soup2.findAll("span", {'class':'view_count'})[0].text
+    if 'k' in viewsRaw:
+        viewsRaw = float(viewsRaw.replace("k", ""))*1000
+    HDviews += int(viewsRaw)
+
+    followersRaw = soup2.findAll("span", {'class':'follower_count'})[0].text
+    if 'k' in followersRaw:
+        followersRaw = float(followersRaw.replace("k", ""))*1000
+    HDprojfollowers += int(followersRaw)
+
+    commentsRaw = soup2.findAll("span", {'class':'comment_count'})[0].text
+    if 'k' in commentsRaw:
+        commentsRaw = float(commentsRaw.replace("k", ""))*1000
+    HDcomments += int(commentsRaw)
+
+
+print 'Total profile followers: ' + str(HDproffollowers)
+print 'Total likes: ' + str(HDlikes)
+print 'Total views: ' + str(HDviews)
+print 'Total projects followers: ' + str(HDprojfollowers)
+print 'Total comments: ' + str(HDcomments)
--- PythonUnitaryTests/youtube.py
+++ PythonUnitaryTests/youtube.py
@@ -1,9 +1,13 @@
 import bs4 as bs
 import urllib2
 
-YTurl = 'https://www.youtube.com/user/ALM37454/about'
+YTurl = 'https://www.youtube.com/user/ALM37454'
+preurl = 'https://www.youtube.com'
+urlList = []
+YTlikes = 0
+YTdislikes =0
 
-req = urllib2.Request(YTurl, headers={ 'User-Agent': 'Mozilla/5.0' })
+req = urllib2.Request(YTurl + '/about', headers={ 'User-Agent': 'Mozilla/5.0' })
 source = urllib2.urlopen(req).read()
 soup = bs.BeautifulSoup(source,'lxml')
 
@@ -11,5 +15,27 @@ YTstats = soup.findAll("span", {'class':'about-stat'})
 YTsuscriptors = int(YTstats[0].b.text.replace(".",""))
 YTviews = int(YTstats[1].b.text.replace(".",""))
 
+
+req2 = urllib2.Request(YTurl + '/videos?live_view=500&flow=list&sort=dd&view=0', headers={ 'User-Agent': 'Mozilla/5.0' })
+source2= urllib2.urlopen(req2).read()
+soup2 = bs.BeautifulSoup(source2,'lxml')
+
+for url in soup2.find_all('a', {'class':'yt-uix-tile-link'}):
+    urlList.append(url.get('href'))
+
+for i in range(len(urlList)):
+    newUrl = preurl + urlList[i]
+    req3 = urllib2.Request(newUrl, headers={ 'User-Agent': 'Mozilla/5.0' })
+    source3= urllib2.urlopen(req3).read()
+    soup3 = bs.BeautifulSoup(source3,'lxml')
+
+    likessection = soup3.findAll('span', {'class':'like-button-renderer'})[0]
+    likestats = likessection.findAll("span", {'class':'yt-uix-button-content'})
+    YTlikes += int(likestats[0].text)
+    YTdislikes += int(likestats[3].text)
+
+
 print 'Total suscriptors: ' + str(YTsuscriptors)
 print 'Total views: ' + str(YTviews)
+print 'Total likes: ' + str(YTlikes)
+print 'Total dislikes: ' + str(YTdislikes)
