3cc005029700cc46c29366493367b4caa122a32c
AngelLM <alm37454@gmail.com>
2017-08-09 21:45:48 +0200

    Instagram function fix

--- functions/instagram.py
+++ functions/instagram.py
@@ -20,18 +20,31 @@ def getStats(IGuser):
     driver = webdriver.Firefox()
     driver.get(IGurl)
 
-    IGTopStats = driver.find_elements_by_class_name("_s53mj")
-    IGTotalUploads = int((IGTopStats[0].text.split())[0])
-    IGTotalFollowers = int((IGTopStats[1].text.split())[0])
-    NowPublicationsRows = driver.find_elements_by_class_name("_myci9")
+    IGTags = driver.find_elements_by_tag_name("span")
+    IGTotalUploads = int(IGTags[3].text)
+    IGTotalFollowers = int(IGTags[5].text)
+
     if IGTotalUploads > 12:
-        button = driver.find_element_by_class_name("_8imhp")
+        FindButton = driver.find_elements_by_tag_name("a")
+        for but in FindButton:
+            if "?max_id" in but.get_attribute("href"):
+                button = but
         button.click()
 
-    while len(driver.find_elements_by_class_name("_myci9")) < math.ceil(IGTotalUploads/3.0):
+    urls = driver.find_elements_by_tag_name("a")
+    for i in urls:
+        if "?taken-by" in i.get_attribute("href"):
+            parent1=i.find_element_by_xpath('..')
+            parent2=parent1.find_element_by_xpath('..')
+            classPhoto = parent1.get_attribute("class").split()[0]
+            print classPhoto
+            classRow = parent2.get_attribute("class")
+            break
+
+    while len(driver.find_elements_by_class_name(classRow)) < math.ceil(IGTotalUploads/3.0):
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
-    photos = driver.find_elements_by_class_name("_8mlbc")
+    photos = driver.find_elements_by_class_name(classPhoto)
 
     for f in photos:
          IGurls.append(f.find_element_by_tag_name("a").get_attribute("href"))
